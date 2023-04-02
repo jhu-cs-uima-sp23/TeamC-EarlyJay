1da00849ddf1420255ea6fefc5c88d76b8e55f20
Yulun <yulunliss001@gmail.com>
2023-04-02 14:40:10 -0400

    migrated all to working bottom nav

--- app/src/main/java/com/example/empty/Map_frag.java
+++ app/src/main/java/com/example/empty/Map_frag.java
@@ -1,53 +1,43 @@
-package com.example.a5_sample.ui.map;
+package com.example.empty;
 
 import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
+
+import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.PopupMenu;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
+import com.example.empty.databinding.FragmentMapBinding;
 
-import com.example.a5_sample.MainActivity;
-import com.example.a5_sample.NewPark;
-import com.example.a5_sample.R;
-import com.example.a5_sample.databinding.FragmentChatBinding;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+public class Map_frag extends Fragment {
 
-public class MapFragment extends Fragment {
-
-    private FragmentChatBinding binding;
+    private FragmentMapBinding binding;
 
     Context cntx;
 
     public boolean settingOff = true;
 
     private MainActivity myact;
-
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-
-
-        View myview = inflater.inflate(R.layout.fragment_map, container, false);
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View myView = inflater.inflate(R.layout.fragment_map, container, false);
 
         // ADDED THIS LINE TO AVOID USING THE ChatViewModel class
-           //  binding.textChat.setText("This is the map tab.");
+        //  binding.textChat.setText("This is the map tab.");
 
         cntx = getActivity().getApplicationContext();
         myact = (MainActivity) getActivity();
 
-        ImageButton setting = myview.findViewById(R.id.setting_button);
-        ImageButton search = myview.findViewById(R.id.search_button);
+        ImageButton setting = myView.findViewById(R.id.setting_button);
+//        ImageButton search = myView.findViewById(R.id.search_button);
 
         setting.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -82,23 +72,20 @@ public class MapFragment extends Fragment {
                     }
                 });
 
-                    MenuInflater inflater = popup.getMenuInflater();
-                    inflater.inflate(R.menu.setting_map_frag_menu, popup.getMenu());
-                    popup.show();
+                MenuInflater inflater = popup.getMenuInflater();
+                inflater.inflate(R.menu.setting_map_frag_menu, popup.getMenu());
+                popup.show();
             }
         });
 
 
-        return myview;
-
+        return myView;
 
     }
-
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
     }
-
 }
