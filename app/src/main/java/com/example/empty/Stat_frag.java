1c6994b434a75a1533cd884f9119e75bc44e99eb
bellaxia <zxia15@jh.edu>
2023-04-03 17:52:17 -0400

    Added Progress Bar

--- app/src/main/java/com/example/empty/Stat_frag.java
+++ app/src/main/java/com/example/empty/Stat_frag.java
@@ -8,12 +8,65 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.empty.databinding.FragmentStatBinding;
+
+import me.tankery.lib.circularseekbar.CircularSeekBar;
+
 public class Stat_frag extends Fragment {
+
+    private FragmentStatBinding binding;
+
+    private CircularSeekBar progressCircular;
+
+    private final int MAX = 120;
+
+    private int factorProgress;
+
+    private boolean mIsDrag;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_stat, container, false);
+
+        binding = FragmentStatBinding.inflate(inflater, container, false);
+
+        progressCircular = binding.circularSeekBar;
+
+
+        progressCircular.setMax(MAX);
+        factorProgress = 12;
+
+        progressCircular.setOnSeekBarChangeListener(
+                new CircularSeekBar.OnCircularSeekBarChangeListener() {
+
+                    @Override
+                    public void onProgressChanged(CircularSeekBar circularSeekBar,
+                                                  float progress, boolean fromUser) {
+                        if (progress <= 0) {
+                            factorProgress = 0;
+                        } else if (progress >= 120){
+                            factorProgress = 24;
+                        } else {
+                            factorProgress = (int) (progress / 5);
+                        }
+                        binding.countNum.setText(factorProgress * 5 + " min");
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(CircularSeekBar seekBar) {
+
+
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(CircularSeekBar seekBar) {
+
+                    }
+                });
+
+        return binding.getRoot();
     }
+
+
 }
