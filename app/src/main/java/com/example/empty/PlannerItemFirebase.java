42aa26009d0cbc5f6d18ddf87d5452cc67078d85
bellaxia <zxia15@jh.edu>
2023-04-16 21:00:41 -0400

    Edited some date problem

--- app/src/main/java/com/example/empty/PlannerItemFirebase.java
+++ app/src/main/java/com/example/empty/PlannerItemFirebase.java
@@ -1,5 +1,7 @@
 package com.example.empty;
-
+import android.annotation.SuppressLint;
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.util.Log;
 
 import java.text.ParseException;
@@ -8,18 +10,97 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class PlannerItemFirebase {
+    String title;
+    String startTime;
+    int duration;
+    String endTime;
+    int workType;
+    int notification = -1;
+    Boolean pinned = false;
+    int cardBackgroundColor;
 
-    private PlannerItemModel plannerItemModel;
+    int status = 0;
 
-    // default = 0; success = 1; fail = 2; miss = 3
-    private int status;
-
-    private DateStr dateStr;
-
-    public PlannerItemFirebase(PlannerItemModel plannerItemModel, String dateStr, int status) {
-      this.plannerItemModel = plannerItemModel;
-      this.status = status;
-      this.dateStr = new DateStr(dateStr);
+    String dateStr;
+    public PlannerItemFirebase(String title, String startTime, int duration, int workType,
+                            int notification, int color, String dateStr){
+        this.title = title;
+        this.startTime = startTime;
+        Log.d("check", startTime);
+        this.duration = duration;
+        this.workType = workType;
+        this.notification = notification;
+        this.endTime = getEndTime(startTime, duration);
+        this.cardBackgroundColor = color;
+        if(title.equals("")) {
+            switch (workType) {
+                case R.drawable.circle_dashed_6_xxl:
+                    this.title = "Work";
+                    break;
+                case R.drawable.yellows:
+                    this.title = "Class";
+                    break;
+                case R.drawable.triangle_48:
+                    this.title = "Team";
+                    break;
+                case R.drawable.star_2_xxl:
+                    this.title = "Sport";
+                    break;
+                default:
+                    break;
+            }
+        }
+        this.dateStr = dateStr;
     }
 
+    public String getTitle() { return title; }
+
+    public String getstartTime() { return startTime; }
+
+    public int getDuration() { return duration; }
+
+    public String getEndTime() { return endTime; }
+
+    public int getWorkTime() { return workType; }
+
+    public int getNotification() { return notification; }
+
+    public Boolean getPinned() { return pinned; }
+    public void togglePin(){
+        pinned = !pinned;
+    }
+    public void setTitle(String title){
+        this.title = title;
+    }
+    public void setStartTime(String startTime){
+        this.startTime = startTime;
+    }
+    public void setDuration(int duration){
+        this.duration = duration;
+    }
+    public void setWorkType(int workType) {
+        this.workType = workType;
+    }
+    public void setNotification(int notification) {
+        this.notification = notification;
+    }
+    public String getEndTime(String startTime, int duration){
+        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm");
+        Date startDate;
+        try {
+            startDate = formatter.parse(startTime);
+        } catch (ParseException ex) {
+            throw new RuntimeException(ex);
+        }
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(startDate);
+        calendar.add(Calendar.MINUTE, duration);
+        Date endDate = calendar.getTime();
+        String formatDate = formatter.format(endDate);
+        if(formatDate.charAt(0)=='0'){
+            formatDate = formatDate.substring(1);
+        }
+        Log.d("check", formatDate);
+        return formatDate;
+    }
 }
